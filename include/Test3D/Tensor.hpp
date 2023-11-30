6278ef2298a5706d12797d72192d34fb6d5f941b
ZhenhLi <907880745@qq.com>
2023-11-30 23:30:09 +0800

    init library
    Add Open3D dependency.

---
+++ include/Test3D/Tensor.hpp
@@ -0,0 +1,19 @@
+#ifndef T3D_TENSOR_HPP_
+#define T3D_TENSOR_HPP_
+
+namespace T3D {
+
+class Tensor {
+ public:
+  Tensor() {}
+  ~Tensor() {}
+};
+
+void outputTensor() {
+
+  return ;
+}
+
+}
+
+#endif
