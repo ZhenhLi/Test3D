4d3f06fdb88b4c95640d0408052ae27c85cb7a7b
ZhenhLi <907880745@qq.com>
2024-02-07 16:02:26 +0800

    add LibTorch error

---
+++ src/testLibTorch.cpp
@@ -0,0 +1,11 @@
+// #include <torch/torch.h>
+// #include <iostream>
+
+// int main() {
+//   std::cout << "---------- test libtorch ----------" << std::endl;
+
+// //   torch::Tensor tensor = torch::eye(3);
+//   torch::Tensor t = torch::rand({2, 3});
+//   std::cout << t << std::endl;
+//   return 0;
+// }
